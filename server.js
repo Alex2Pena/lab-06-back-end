56353adbc8c72a9b57e12f31391bad11f6ae6d8e
Alex2Pena <attn.Alex.P@gmail.com>
2020-02-24 12:29:41 -0800

    added API key

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/trails', (request, response) => {
 app.get('/movies', (request, response) => {
   let location = request.query.search_query;
   console.log(request.search_query);
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.THEMOVIEDB_API}&language=en-US&query=${location}&page=1&include_adult=false`;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${location}&page=1&include_adult=false`;
 
   superagent.get(url)
     .then (results => {
